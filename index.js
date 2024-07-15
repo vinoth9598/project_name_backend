a1788f2b5afb55668e58b99fcf76a3195aecd75f
vinoth9598 <pvinoth9598@gmail.com>
2024-07-15 09:37:01 -0700

    finish

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const connection =require('./database/db');
+const cors=require('cors');
+
+const saveSentEmails = require('./controller/email-controller.js')
+const getEmails = require('./controller/getEmails.js');
+const moveEmailsToBin=require( "./controller/moveBin.js");
+const toggleStarredEmails=require('./controller/starredEmails.js');
+const deleteEmails = require('./controller/deleteEmails.js');
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/save',saveSentEmails);
+app.use('/',getEmails);
+app.use('/save-draft',saveSentEmails);
+app.use('/',moveEmailsToBin);
+app.use('/starred',toggleStarredEmails);
+app.use('/',deleteEmails);
+
+const PORT = 3001;
+
+connection();
+
+app.listen(PORT,()=>{
+    console.log(`server starting on PORT ${PORT}`)
+});
+
